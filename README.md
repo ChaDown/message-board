6da91983861605eb7116f53ac7a2827c612273c9
ChaDown <charliedownes@hotmail.com>
2023-01-20 17:28:03 +0000

    Created a full basic express project.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my first mini-project using node, express, ejs and railway hosting.
+
+The main focus of this project is to really understand how everything nets together in the backend. First I created the app using express generator, adding -ejs as my templating engine.
+
+Then I created an index.js in the routes directory, and in here managed what would be rendered with the "/" and "/new" routes. These use the render method to render the given views in the views directory.
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message board', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  const { message, authorName } = req.body;
+  messages.push({ text: message, user: authorName, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
